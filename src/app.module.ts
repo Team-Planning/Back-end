c1500459ea5d3d26cf8bb1ef9015c247df72c231
Danixoxo69 <daniel.belozo@alumnos.uv.cl>
2025-08-21 14:41:49 -0400

    Ajustes pequeños

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PublicacionesModule } from './publicaciones/publicaciones.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { UsersModule } from './users/users.module';
     // Módulos de la aplicación
     AuthModule,
     UsersModule,
+    PublicacionesModule,
   ],
 })
 export class AppModule {}
