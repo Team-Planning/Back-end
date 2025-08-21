13eefb5e8fd67b5b585a07e78111b710b539ee90
Danixoxo69 <daniel.belozo@alumnos.uv.cl>
2025-08-21 14:00:38 -0400

    Ajustes

--- src/publicaciones/interfaces/publicacion.interface.ts
+++ src/publicaciones/interfaces/publicacion.interface.ts
@@ -4,8 +4,8 @@ export interface Publicacion extends Document {
   titulo: string;
   descripcion: string;
   precio: number;
-  categoria: string; // ObjectId como string
-  imagenes: string[]; // Array de ObjectId como string
+  categoria: string;
+  imagenes: string[];
   vendedorId: string;
   estado: 'borrador' | 'revision' | 'activo' | 'pausado' | 'vendida' | 'rechazada';
   fechaCreacion: Date;
