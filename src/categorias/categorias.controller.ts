fbb9408c844d9f7969a685f0dc28fb259451e12d
Danixoxo69 <daniel.belozo@alumnos.uv.cl>
2025-10-14 12:07:43 -0300

    Fix categorias.controller.ts merge conflicts - keep complete HEAD version with all endpoints

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -6,31 +6,20 @@ import {
   Delete,
   Param,
   Body,
-<<<<<<< HEAD
   Patch,
   HttpCode,
   HttpStatus,
-=======
-  Patch
->>>>>>> 087c206160ac6d8e3482efcdbc4f7b9a2ea473fa
 } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
 
-<<<<<<< HEAD
 @Controller('categorias')
-=======
-@Controller('Categorias')
->>>>>>> 087c206160ac6d8e3482efcdbc4f7b9a2ea473fa
 export class CategoriasController {
   constructor(private readonly categoriasService: CategoriasService) {}
 
   @Post()
-<<<<<<< HEAD
   @HttpCode(HttpStatus.CREATED)
-=======
->>>>>>> 087c206160ac6d8e3482efcdbc4f7b9a2ea473fa
   async crear(@Body() dto: CreateCategoriaDto) {
     return this.categoriasService.crear(dto);
   }
@@ -40,14 +29,11 @@ export class CategoriasController {
     return this.categoriasService.listarTodas();
   }
 
-<<<<<<< HEAD
   @Get('activas')
   async listarActivas() {
     return this.categoriasService.listarActivas();
   }
 
-=======
->>>>>>> 087c206160ac6d8e3482efcdbc4f7b9a2ea473fa
   @Get(':id')
   async obtenerPorId(@Param('id') id: string) {
     return this.categoriasService.obtenerPorId(id);
@@ -59,7 +45,6 @@ export class CategoriasController {
   }
 
   @Delete(':id')
-<<<<<<< HEAD
   @HttpCode(HttpStatus.OK)
   async eliminar(@Param('id') id: string) {
     return this.categoriasService.eliminar(id);
@@ -74,9 +59,4 @@ export class CategoriasController {
   async desactivar(@Param('id') id: string) {
     return this.categoriasService.activarDesactivar(id, false);
   }
-=======
-  async eliminar(@Param('id') id: string) {
-    return this.categoriasService.eliminar(id);
-  }
->>>>>>> 087c206160ac6d8e3482efcdbc4f7b9a2ea473fa
 }
